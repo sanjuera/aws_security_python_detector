8f7a8b3d5e53a3b67db26612f1447b362d4b3831
Sanju raj <sanjuera@amazon.com>
2025-09-25 13:29:22 +0530

    Add JavaScript files batch 2: 5 security-related JS files

---
+++ javascript/random_samples/xxe_node.js
@@ -0,0 +1,71 @@
+
+const libxmljs = require('libxmljs');
+// {fact rule=xml-external-entity@v1.0 defects=1}
+app.get('/noent', function (req, res) {
+    // entity expansion
+    // ruleid:node_xxe
+    libxmljs.parseXml(req.param("xml"), { noent: true });
+});
+// {/fact}
+
+
+
+// {fact rule=xml-external-entity@v1.0 defects=1}
+app.get('/sax', function (req, res) {
+    // SAX parser expands external entities
+    // ruleid:node_xxe
+    const parser = new libxmljs.SaxParser();
+    const x = 1
+    parser.parseString(req.param("xml"));
+});
+
+// {/fact}
+
+
+// {fact rule=xml-external-entity@v1.0 defects=1}
+app.get('/saxpush/parser', function (req, res) {
+    // SAX parser expands external entities
+    // ruleid:node_xxe
+    const parser = new libxmljs.SaxPushParser();
+    const x = 1
+    parser.push(req.param("some-xml"));
+});
+
+// {/fact}
+
+
+// {fact rule=xml-external-entity@v1.0 defects=1}
+app.get('/sax', function (req, res) {
+    // SAX parser expands external entities
+    const parser = new libxmljs.SaxParser();
+    const x = 1
+    // ruleid:node_xxe
+    var products = parser.parseXmlString(req.files.products.data, { noent: true, noblanks: true })
+})
+
+// {/fact}
+
+// {fact rule=xml-external-entity@v1.0 defects=1}
+const express = require('express')
+const libxmljs = require('libxml')
+const db = require('db');
+const router = express.Router()
+
+router.post('/upload-products', (req, res) => {
+    // ruleid:node_xxe
+    const XMLfile = req.files.products.data;
+    const products = libxmljs.parseXmlString(XMLfile, { noent: true, noblanks: true })
+
+    products.root().childNodes().forEach(product => {
+        let newProduct = new db.Product()
+        newProduct.name = product.childNodes()[0].text()
+        newProduct.description = product.childNodes()[3].text()
+        newProduct.save()
+    });
+
+    res.send('Thanks')
+})
+
+module.exports = router
+
+// {/fact}
